05cb59961915066a6becfb1b41a18e417b069a46
박수정 <zxzx4342@naver.com>
2021-09-12 16:08:14 +0900

    주석 삭제

--- app/src/main/java/com/neppplus/jickbbangcopy_210912/datas/RoomDatas.kt
+++ app/src/main/java/com/neppplus/jickbbangcopy_210912/datas/RoomDatas.kt
@@ -13,9 +13,6 @@ class RoomDatas(
 
     fun getFormattedFloor() : String {
 
-//        1. 양수인 경우 (1, 3, 5..) => "floor층"
-//        2. 0인 경우 => "반지하"
-//        3. 음수인 경우 (-1, -2..) => "지하 ?층"
 
         if (this.floor > 0) {
             return "${this.floor}층"
@@ -33,18 +30,14 @@ class RoomDatas(
 
     fun getFormatTedPrice() : String {
 
-//        1. 1억이 안넘는 경우 ( 가격 < 10000 )
-//        2. 그 외 : 1억 이상 => ?억 ???
         if (this.price < 10000) {
 
-//            8000 => "8,000" 으로 변경.
             val formattedStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
 
             return formattedStr
         }
         else {
 
-//            75000 => "7억 5,000"
             val bilion = this.price / 10000
             val rest = this.price % 10000
 
