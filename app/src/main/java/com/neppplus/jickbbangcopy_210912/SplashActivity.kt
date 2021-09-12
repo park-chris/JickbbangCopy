af055bdd7a8aa25a566bff82b052905808a1b3a7
박수정 <zxzx4342@naver.com>
2021-09-12 16:05:46 +0900

    메인화면으로 이동 -> 너무 바로 이동되어서 부자연스럽다.   => 3초정도 지연시켜보자.

--- app/src/main/java/com/neppplus/jickbbangcopy_210912/SplashActivity.kt
+++ app/src/main/java/com/neppplus/jickbbangcopy_210912/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.jickbbangcopy_210912
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myIntent = Intent(this, MainActivity::class.java)
+
+//        3초 뒤에 -> 화면 전환 코드 실행 시키고 싶다.
+//        코드 실행을 3초 지연시키는 방법?
+
+
+        startActivity(myIntent)
+        finish()
     }
 }
