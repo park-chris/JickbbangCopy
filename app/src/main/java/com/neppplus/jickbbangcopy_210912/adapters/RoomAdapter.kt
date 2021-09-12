30c88cfc28af7813abf552f6dcf3866c32685492
박수정 <zxzx4342@naver.com>
2021-09-12 11:43:01 +0900

    리스트 아이템 기초 추가까지만.  - 모양은 새로 그려줄 예정

---
+++ app/src/main/java/com/neppplus/jickbbangcopy_210912/adapters/RoomAdapter.kt
@@ -0,0 +1,11 @@
+package com.neppplus.jickbbangcopy_210912.adapters
+
+import com.neppplus.jickbbangcopy_210912.datas.RoomDatas
+
+class RoomAdapter (
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomDatas>) : ArrayList<RoomDatas>{
+
+
+}
---
+++ app/src/main/java/com/neppplus/jickbbangcopy_210912/datas/RoomDatas.kt
@@ -0,0 +1,6 @@
+package com.neppplus.jickbbangcopy_210912.datas
+
+class RoomDatas(
+    val price: Int,
+    val address: String,
+    val description: String) { }
